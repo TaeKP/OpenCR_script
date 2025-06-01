e3c5556884381b406bb1cd24f01e92d12efbc9c2
TaeKP <pskitipat@gmail.com>
2025-06-02 00:17:12 +0700

    Comparing 3 decreasing emigration scenarios with baseline scenario

--- FishingCat_PVA_Simulations.R
+++ FishingCat_PVA_Simulations.R
@@ -1122,4 +1122,24 @@ ggplot(simSummary_s18, aes(x = Year, group = Model)) +
   scale_fill_brewer(palette = "Dark2") + 
   theme_bw()
 
+## Comparing 3 decreasing emigration scenarios with baseline scenario
+## Combine results and summarise
+simSummary_s16t18 <- rbind(sim_s16, sim_s17, sim_s18, sim_4) %>%
+  dplyr::mutate(PopSize = ifelse(is.na(PopSize), 0, PopSize)) %>%
+  dplyr::group_by(Model, Year) %>%
+  dplyr::summarise(mean_N = mean(PopSize),
+                   median_N = median(PopSize),
+                   sd_N = sd(PopSize),
+                   lCI_N = quantile(PopSize, probs = 0.025),
+                   uCI_N = quantile(PopSize, probs = 0.975),
+                   .groups = "keep") 
+
+## Plot
+ggplot(simSummary_s16t18, aes(x = Year, group = Model)) + 
+  geom_line(aes(y = median_N, color = Model)) + 
+  geom_ribbon(aes(ymin = lCI_N, ymax = uCI_N, fill = Model), alpha = 0.2) + 
+  xlim(1, n_years-1) + 
+  scale_color_brewer(palette = "Dark2") + 
+  scale_fill_brewer(palette = "Dark2") + 
+  theme_bw()
 #-------------------------------------------------------------------------------
