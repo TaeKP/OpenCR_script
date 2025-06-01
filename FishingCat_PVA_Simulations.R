fbab74d09b9d5a8cdd618c480c28ebcd8d82ebae
TaeKP <pskitipat@gmail.com>
2025-06-01 23:11:12 +0700

    Testing the sensitivity analysis scenario 10, survival rate
    10% increasing the survival rate, with the perturbation factor "S" at 1.1
    and comparing the results between scenario 10 and baseline scenario

--- FishingCat_PVA_Simulations.R
+++ FishingCat_PVA_Simulations.R
@@ -736,3 +736,51 @@ ggplot(simSummary_s7t9, aes(x = Year, group = Model)) +
   theme_bw()
 
 #-------------------------------------------------------------------------------
+## Scenario 10
+## Low threat and good management
+## 10% increasing the survival rate 
+## perturbation factor "S" at 1.1
+
+# Running the simulation multiple times
+(results_age_str_s10 <- replicate(simulations, pva_simulation_age_str(initN, 
+                                                                     growth_rate, growth_rate_sd,
+                                                                     survival_rate, survival_rate_sd,
+                                                                     recruitment_rate, recruitment_rate_sd,
+                                                                     init_adultProp, init_adultProp_SD,
+                                                                     carrying_capacity, 
+                                                                     pertFac.S = 1.1, pertFac.f = 1, pertFac.E = 1,
+                                                                     n_years)) )
+
+# Write results as data frames
+# compare with baseline scenario
+
+sim_s10 <- reshape2::melt(apply(results_age_str_s10, c(2, 3), sum)) %>%
+  dplyr::rename(Year = Var1, SimNo = Var2, PopSize = value) %>%
+  dplyr::mutate(Model = "10% survival increased")
+
+#baseline scenario
+sim_4 <- reshape2::melt(apply(results_age_str, c(2, 3), sum)) %>%
+  dplyr::rename(Year = Var1, SimNo = Var2, PopSize = value) %>%
+  dplyr::mutate(Model = "1-year, vital rates & age structure")
+
+## Combine results and summarise
+simSummary_s10 <- rbind(sim_s10, sim_4) %>%
+  dplyr::mutate(PopSize = ifelse(is.na(PopSize), 0, PopSize)) %>%
+  dplyr::group_by(Model, Year) %>%
+  dplyr::summarise(mean_N = mean(PopSize),
+                   median_N = median(PopSize),
+                   sd_N = sd(PopSize),
+                   lCI_N = quantile(PopSize, probs = 0.025),
+                   uCI_N = quantile(PopSize, probs = 0.975),
+                   .groups = "keep") 
+
+## Plot
+ggplot(simSummary_s10, aes(x = Year, group = Model)) + 
+  geom_line(aes(y = median_N, color = Model)) + 
+  geom_ribbon(aes(ymin = lCI_N, ymax = uCI_N, fill = Model), alpha = 0.2) + 
+  xlim(1, n_years-1) + 
+  scale_color_brewer(palette = "Dark2") + 
+  scale_fill_brewer(palette = "Dark2") + 
+  theme_bw()
+
+#-------------------------------------------------------------------------------
