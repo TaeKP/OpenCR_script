b435adc46752fb48389e0a3273549e42a2da93dc
TaeKP <pskitipat@gmail.com>
2025-09-02 17:24:02 +0700

    Testing the sensitivity analysis scenario 20, survival and recruitment rate
    10% increasing both the survival and recruitment rate, with the perturbation factor both "S and f" at 1.1 and comparing the results between scenario 20 and baseline scenario

--- FishingCat_PVA_Simulations.R
+++ FishingCat_PVA_Simulations.R
@@ -1333,3 +1333,45 @@ results_no_na_s19 <- is.na(results_age_str_s19[,n_years,] == 0)
 (extinction_probability_age_str_s19 <- mean(results_no_na_s19))
 
 #-------------------------------------------------------------------------------
+## Scenario 20 (additional scenario)
+## Good management on land use change and low threat (illegal-killing) could cause high survival and recruitment
+## Therefore, the survival and recruitment rate must be increased
+## 10% increasing both the survival and recruitment rate 
+## perturbation factor both "S and f" at 1.1
+
+# Running the simulation multiple times
+(results_age_str_s20 <- replicate(simulations, pva_simulation_age_str(initN, 
+                                                                      growth_rate, growth_rate_sd,
+                                                                      survival_rate, survival_rate_sd,
+                                                                      recruitment_rate, recruitment_rate_sd,
+                                                                      init_adultProp, init_adultProp_SD,
+                                                                      carrying_capacity, 
+                                                                      pertFac.S = 1.1, pertFac.f = 1.1, pertFac.E = 1,
+                                                                      n_years)) )
+
+# Write results as data frames
+# compare with baseline scenario
+
+sim_s20 <- reshape2::melt(apply(results_age_str_s20, c(2, 3), sum)) %>%
+  dplyr::rename(Year = Var1, SimNo = Var2, PopSize = value) %>%
+  dplyr::mutate(Model = "10% survival/recruitment increased")
+
+## Combine results and summarise
+simSummary_s20 <- rbind(sim_s20, sim_4) %>%
+  dplyr::mutate(PopSize = ifelse(is.na(PopSize), 0, PopSize)) %>%
+  dplyr::group_by(Model, Year) %>%
+  dplyr::summarise(mean_N = mean(PopSize),
+                   median_N = median(PopSize),
+                   sd_N = sd(PopSize),
+                   lCI_N = quantile(PopSize, probs = 0.025),
+                   uCI_N = quantile(PopSize, probs = 0.975),
+                   .groups = "keep") 
+
+## Plot
+ggplot(simSummary_s20, aes(x = Year, group = Model)) + 
+  geom_line(aes(y = median_N, color = Model)) + 
+  geom_ribbon(aes(ymin = lCI_N, ymax = uCI_N, fill = Model), alpha = 0.2) + 
+  xlim(1, n_years-1) + 
+  scale_color_brewer(palette = "Dark2") + 
+  scale_fill_brewer(palette = "Dark2") + 
+  theme_bw()
