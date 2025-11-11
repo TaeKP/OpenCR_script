9764d3beaf80c292f045b82ce2e3c4f740564f55
TaeKP <pskitipat@gmail.com>
2025-11-11 22:25:06 +0700

    Run function with 1000 simulation samples

--- Sensitivity_Analysis_AgeStructure.R
+++ Sensitivity_Analysis_AgeStructure.R
@@ -41,42 +41,66 @@ popMat <- build_popMat(growth_rate = lambda, growth_rate_sd = SD_growth_rate,
                        init_adultProp = init_adultProp_mean, init_adultProp_SD = init_adultProp_SD,
                        stochastic = stochastic)
 
-#A <- popMat$A
+A <- popMat$A
+
+# Checking the structure
+str(popMat)
+str(A)
 
 ## Run function with 1000 simulation stochastic samples
-popMat2 <- build_popMat(growth_rate = lambda, growth_rate_sd = SD_growth_rate,
+result_popMat <- replicate(nsim, build_popMat(growth_rate = lambda, growth_rate_sd = SD_growth_rate,
                                               survival_rate = S, survival_rate_sd = SD_survival_rate,
                                               recruitment_rate = f, recruitment_rate_sd = SD_recruitment,
                                               init_adultProp = init_adultProp_mean, init_adultProp_SD = init_adultProp_SD,
-                                              stochastic = nsim)
+                                              stochastic = stochastic))
 
-A1 <- popMat2$A
+
+# Checking structure
+str(result_popMat)
+
+# indexing of each matrix; first 10 slices
+result_popMat[c(1, 7, 13, 19, 25, 31, 37, 43, 49, 55)]
+
+# construct the for loop to store the nsim values 
+
+# Selecting matrix every 6 values
+selected_indices <- seq(1, length(result_popMat), by = 6)
+
+# Checking the size of matrix 
+nrow_mat <- nrow(result_popMat[[selected_indices[1]]])
+ncol_mat <- ncol(result_popMat[[selected_indices[1]]])
+
+# Creating empty array
+nsim_array <- array(NA, dim = c(nrow_mat, ncol_mat, length(selected_indices)))
+
+# Filling every matrix in each slice
+for (k in seq_along(selected_indices)) {
+  nsim_array[,,k] <- result_popMat[[selected_indices[k]]]
+}
+
+# Checking the result
+print(dim(nsim_array))    # array size 2*2*10
+print(nsim_array[,,1:10])  # slice 1 to 10
+print(nsim_array[,,100])   # slice 100
+print(nsim_array[,,1000])   # slice 1000
+
 
 # ASYMPTOTIC ANALYSIS #
 #---------------------#
 
 ## Asymptotic population growth rate 
 #  = dominant right eigenvalue of matrix
-#lambda <- popbio::lambda(A)
+lambda <- popbio::lambda(A)
 
 ## Matrix element sensitivities
 # = derivative of lambda with respect to each matrix element
 # = potential effect a small absolute change in matrix element could have on population growth rate
-#sens.ME <- popbio::sensitivity(A)
+sens.ME <- popbio::sensitivity(A)
 
 ## Matrix element elasticities (= relative sensitivities)
 # = sensitivity multiplied by (matrix element/lambda)
 # = potential effect a small relative change in matrix element could have on population growth rate
-#elas.ME <- popbio::elasticity(A)
-
-#  = dominant right eigenvalue of matrix for matrix A1
-lambda1 <- popbio::lambda(A1)
-
-## Matrix element sensitivities for matrix A1
-sens.ME1 <- popbio::sensitivity(A1)
-
-## Matrix element elasticities for matrix A1
-elas.ME1 <- popbio::elasticity(A1)
+elas.ME <- popbio::elasticity(A)
 
 
 ## Vital rate sensitivities
@@ -97,7 +121,7 @@ derivA_S1yr[2, 1] <- 1
 derivA_S1yr[2, 2] <- 1 
 derivA_S1yr[1, 2] <- 0
 
-sens_S1yr <- sum(derivA_S1yr*sens.ME1)
+sens_S1yr <- sum(derivA_S1yr*sens.ME)
 
 # Derivative matrix for E1yr
 derivA_E1yr <- matrix(NA, nrow = 2, ncol = 2)
@@ -106,7 +130,7 @@ derivA_E1yr[2, 1] <- -1
 derivA_E1yr[2, 2] <- -1
 derivA_E1yr[1, 2] <- 0
 
-sens_E1yr <- sum(derivA_E1yr*sens.ME1)
+sens_E1yr <- sum(derivA_E1yr*sens.ME)
 
 # Derivative matrix for f1yr_ad
 derivA_f1yr <- matrix(NA, nrow = 2, ncol = 2)
@@ -115,7 +139,7 @@ derivA_f1yr[2, 1] <- 0
 derivA_f1yr[2, 2] <- 0 
 derivA_f1yr[1, 2] <- 1
 
-sens_f1yr <- sum(derivA_f1yr*sens.ME1)
+sens_f1yr <- sum(derivA_f1yr*sens.ME)
 
 ## Vital rate elasticities
 # = sensitivity multiplied by (vital rate/lambda)
